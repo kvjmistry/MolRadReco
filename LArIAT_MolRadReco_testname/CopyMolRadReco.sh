ea49b6414541ec59511335ac06bceea4b33a1645
Krishan Mistry <kvjmistry@ppdhcp233.hep.manchester.ac.uk>
2018-07-20 11:33:38 +0100

    more renaming

--- LArIAT_MolRadReco_testname/CopyMolRadReco.sh
+++
@@ -1,9 +0,0 @@
-# A code which downloads the LArIAT Moliere radius code from the gpvms and adds it to 
-scp kmistry@uboonegpvm01.FNAL.GOV:/uboone/app/users/kmistry/LArIAT_MolRadReco/srcs/lariatsoft/LArIATAnaModule/MolRadReco_module.cc  .;
-scp kmistry@uboonegpvm01.FNAL.GOV:/uboone/app/users/kmistry/LArIAT_MolRadReco/srcs/lariatsoft/LArIATAnaModule/molradrecoorig.fcl  .;
-
-git add *;
-
-echo "Now run: git commit -m Enter your update here to save the code" 
-echo "Then Run: git push origin master"
-
