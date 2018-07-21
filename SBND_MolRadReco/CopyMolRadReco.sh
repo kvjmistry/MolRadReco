588a4dfeaf247937fc07805f1a0e3862239dd07d
Krishan Mistry <kvjmistry@krishans-mbp.home>
2018-07-21 16:24:43 +0100

    updated the .gitignore to remove the ds store file

--- SBND_MolRadReco/CopyMolRadReco.sh
+++
@@ -1,8 +0,0 @@
-# A code which downloads the SBND Moliere radius code from the gpvms and adds it to my github page 
-scp kmistry@sbndgpvm01.FNAL.GOV:/sbnd/app/users/kmistry/sbndcode_v06_79_00/srcs/sbndcode/sbndcode/MolRadReco/*  .;
-
-git add *;
-
-echo "Now run: git commit -m Enter your update here to save the code" 
-echo "Then Run: git push origin master"
-
